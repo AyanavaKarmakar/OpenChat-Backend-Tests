78b8912d49ff9d4d166fb3cbff48935917a59ada
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-09 23:12:35 +0530

    feat: add test for checking greeting message

--- GreetingControllerTests.cs
+++ GreetingControllerTests.cs
@@ -20,5 +20,27 @@ namespace OpenChat_Backend.Greeting.Controllers.Tests
             var okResponse = Assert.IsType<OkObjectResult>(response);
             Assert.Equal(200, okResponse.StatusCode);
         }
+
+        [Fact]
+        public void Get_IsResultOk_WithMessage()
+        {
+            // Arrange
+            var controller = new GreetingController();
+
+            // Act
+            var response = controller.Get();
+
+            // Assert
+            Assert.NotNull(response);
+
+            var okResponse = Assert.IsType<OkObjectResult>(response);
+            Assert.Equal(200, okResponse.StatusCode);
+
+            // is this really how it's supposed to be done?
+            var message = Assert.IsType<string>(
+                okResponse.Value?.GetType().GetProperty("message")?.GetValue(okResponse.Value));
+
+            Assert.Equal("Welcome to OPENCHAT!", message);
+        }
     }
 }
