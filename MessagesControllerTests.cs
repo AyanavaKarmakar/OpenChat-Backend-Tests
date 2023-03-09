8cf6cb952f7a1007465ca882da9f7c946c03e21d
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-10 01:11:48 +0530

    feat: add test for getting all messages

---
+++ MessagesControllerTests.cs
@@ -0,0 +1,68 @@
+using Chat.Models;
+using Microsoft.AspNetCore.Mvc;
+using OpenChat.Message.Controllers;
+using Microsoft.EntityFrameworkCore;
+
+namespace OpenChat_Backend.Messages.Controllers.Tests
+{
+    public class MessagesControllerTests
+    {
+        [Fact]
+        public async Task GetAllMessages_ReturnsOkObjectResult_WithListOfMessages()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ChatDB>()
+                        .UseInMemoryDatabase(databaseName: "TestDB")
+                        .Options;
+
+            using (var context = new ChatDB(options))
+            {
+                var messages = new List<Message>
+                {
+                    new Message
+                    {
+                        Id = 1,
+                        Sender = "user1",
+                        MessageContent = "Hello",
+                        Timestamp = DateTime.Now
+                    },
+                    new Message
+                    {
+                        Id = 2,
+                        Sender = "user2",
+                        MessageContent = "Hi",
+                        Timestamp = DateTime.Now.AddSeconds(5)
+                    },
+                    new Message
+                    {
+                        Id = 3,
+                        Sender = "user1",
+                        MessageContent = "How are you?",
+                        Timestamp = DateTime.Now.AddSeconds(10)
+                    }
+                };
+
+                context.Messages.AddRange(messages);
+                await context.SaveChangesAsync();
+            }
+
+            using (var context = new ChatDB(options))
+            {
+                var controller = new MessageController(context);
+
+                // Act
+                var result = await controller.GetAllMessages();
+
+                // Assert
+                Assert.NotNull(result);
+
+                var okResult = Assert.IsType<OkObjectResult>(result);
+                Assert.Equal(200, okResult.StatusCode);
+
+                var messages = Assert.IsAssignableFrom<IEnumerable<Message>>(okResult.Value);
+                Assert.Equal(3, messages.Count());
+                Assert.Equal("How are you?", messages.First().MessageContent);
+            }
+        }
+    }
+}
