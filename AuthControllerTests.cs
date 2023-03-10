56ace31933a56f4cf229c4f5f7721effcc6bb5a9
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-03-11 00:24:28 +0530

    feat: setup tests for registering user

---
+++ AuthControllerTests.cs
@@ -0,0 +1,102 @@
+using Chat.Models;
+using Chat.UserDto;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using OpenChat.Auth.Controllers;
+
+namespace OpenChat.Auth.Tests
+{
+    public class AuthControllerTests
+    {
+        private readonly ChatDB _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthControllerTests()
+        {
+            var options = new DbContextOptionsBuilder<ChatDB>()
+                .UseInMemoryDatabase(databaseName: "ChatDB")
+                .Options;
+
+            _context = new ChatDB(options);
+
+            var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new[]
+                {
+                    new KeyValuePair<string, string>("AppSettings:Token", "mysupersecretkey")
+                })
+                .Build();
+
+            _configuration = configuration;
+        }
+
+        private async Task<ChatDB> ArrangeTestData()
+        {
+            // Clear the database to ensure that there are no duplicate keys
+            await _context.Database.EnsureDeletedAsync();
+            await _context.Database.EnsureCreatedAsync();
+
+            var users = new List<UserEntity>
+            {
+                new UserEntity
+                {
+                    Id = 1,
+                    Username = "johndoe",
+                    PasswordHash = new byte[64],
+                    PasswordSalt = new byte[128]
+                },
+                new UserEntity
+                {
+                    Id = 2,
+                    Username = "janedoe",
+                    PasswordHash = new byte[64],
+                    PasswordSalt = new byte[128]
+                }
+            };
+
+            _context.Users.AddRange(users);
+            await _context.SaveChangesAsync();
+
+            return _context;
+        }
+
+        [Fact]
+        public async Task Register_ReturnsBadRequest_WhenUsernameIsAlreadyTaken()
+        {
+            using (var context = await ArrangeTestData())
+            {
+                // Arrange
+                var controller = new AuthController(context, _configuration);
+                var request = new UserDto
+                {
+                    Username = "johndoe",
+                    Password = "testpassword"
+                };
+
+                // Act
+                var result = await controller.Register(request);
+
+                // Assert
+                Assert.NotNull(result);
+            }
+        }
+
+        [Fact]
+        public async Task Register_ReturnsOk_WhenRegistrationIsSuccessful()
+        {
+            // Arrange
+            var controller = new AuthController(_context, _configuration);
+
+            var request = new UserDto
+            {
+                Username = "janedoe",
+                Password = "testpassword"
+            };
+
+            // Act
+            var result = await controller.Register(request);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
